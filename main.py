c0221e9361601fb0051986dda3e5af8daeff462d
Jake Hash <1508098+jhash@users.noreply.github.com>
2024-10-16 15:44:16 -0400

    Add while loop

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ FPS = 60
 FramePerSec = pygame.time.Clock()
 
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
+pygame.display.set_caption("Jake")
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -31,3 +31,21 @@ class platform(pygame.sprite.Sprite):
 
 PT1 = platform()
 P1 = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(PT1)
+all_sprites.add(P1)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    displaysurface.fill((255,255,255))
+
+    for entity in all_sprites:
+        displaysurface.blit(entity.surf, entity.rect)
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
